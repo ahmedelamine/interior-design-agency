6b6e4bb4a9c0d4dca4a5f5bd9d104419c716bd10
ahmedelamine <ahmedelamine5@gmail.com>
2023-07-09 11:14:17 +0100

    extending container

--- main.js
+++ main.js
@@ -5,4 +5,19 @@ menuBtn.addEventListener("click", () => {
     links.classList.toggle("show")
 })
 
+// Extending panel
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
 
